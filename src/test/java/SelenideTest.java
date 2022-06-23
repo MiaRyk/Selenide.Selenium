e250509b079f6276e5d8480920bac3c62344f958
Mariia R <mariadata@gmail.com>
2022-06-23 20:23:45 +0300

    HW Selemide/TestNG

---
+++ src/test/java/SelenideTest.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.cssSelector;
+
+public class SelenideTest {
+    @Test
+    public void deliveryOutsideUkraine (){
+        Configuration.browserSize="2100x1080";
+        open("https://www.ukrposhta.ua/ua");
+        $(By.xpath("//*[@id='mizhnarodni1_main']")).scrollTo();
+        $(By.xpath("//*[@id='mizhnarodni1_main']")).hover().click();
+        $(By.cssSelector("#calculate1")).click();
+        $(By.xpath("//button[text()='Дякую, ознайомлений (-а)']")).should(appear).click();
+        $(By.xpath("//*[@id='toInternationalCalc']")).click();
+        $(By.cssSelector("#toDomesticCalc")).shouldBe(visible);
+        $(By.cssSelector("#departureCntry")).should(editable).doubleClick();
+        $(By.cssSelector("#departureCntry")).setValue("Німеччина").pressEnter();
+        $(By.cssSelector("#main app-form-h6:nth-child(4)")).scrollTo();
+        $(By.cssSelector("#weight")).setValue("2").pressEnter();
+        $(By.cssSelector("#lenght")).setValue("20").pressEnter();
+        $(By.cssSelector("#height")).setValue("20").pressEnter();
+        $(By.cssSelector("#width")).setValue("20").pressEnter();
+        $(By.cssSelector("#ground")).scrollTo().click();
+        $(By.cssSelector("#main app-form-submit button")).shouldBe(enabled).shouldHave(exactText("Розрахувати вартість")).click();;
+    }
+}
